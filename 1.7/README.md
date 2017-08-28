382addf5f5e3a3b63f0a445f0d38c5644ee03bfa
Michael Hausenblas <michael.hausenblas@gmail.com>
2017-08-28 09:46:19 +0100

    adds listing pods to 1.7

--- 1.7/README.md
+++ 1.7/README.md
@@ -1,1 +1,15 @@
-# Using client-go with Kubernetes 1.7
+# Using `client-go` with Kubernetes 1.7
+
+## Listing pods
+
+To list all pods in namespace `abc`, do the following:
+
+```
+clientset, _ := kubernetes.NewForConfig(config)
+pods, _ := clientset.CoreV1().Pods('abc').List(metav1.ListOptions{})
+for _, p := range pods.Items {
+  fmt.Println(p.GetName())
+}
+```
+
+See also  the [standalone example](listing-pods/main.go) for listing pods.
---
+++ 1.7/listing-pods/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	pl, _ := listpods("default")
+	fmt.Println(pl)
+}
+
+func listpods(namespace string) ([]string, error) {
+	var po []string
+	config, err := clientcmd.BuildConfigFromFlags("", os.Getenv("HOME")+"/.kube/config")
+	if err != nil {
+		return po, err
+	}
+	cs, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return po, err
+	}
+	pods, err := cs.CoreV1().Pods(namespace).List(metav1.ListOptions{})
+	if err != nil {
+		return po, err
+	}
+	for _, p := range pods.Items {
+		po = append(po, p.GetName())
+	}
+	return po, nil
+}
